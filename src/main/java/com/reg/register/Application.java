d69bb613f17496fe9a8d4d507deb09590a47f3c5
witch <witchakorn.b@kkumail.com>
2023-03-23 16:31:26 +0700

    version 0.0.0.1 ConnectDB

--- src/main/java/com/reg/register/Application.java
+++ src/main/java/com/reg/register/Application.java
@@ -3,6 +3,8 @@ package com.reg.register;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
@@ -11,6 +13,7 @@ public class Application extends javafx.application.Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("register application");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
